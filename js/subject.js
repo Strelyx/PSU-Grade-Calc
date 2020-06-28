d07963067e74f349a6d8413fc10ca561e2663c47
Strelyx <51486537+Strelyx@users.noreply.github.com>
2020-06-28 16:30:03 +0700

    Add files via upload

--- js/subject.js
+++ js/subject.js
@@ -1,42 +1,120 @@
 // JavaScript Document
-var firstTerm = false;
-var secondTerm = false;
+var firstTerm = true
+var secondTerm = true
 
-var isLowerSection = false;
+var btn_firstTerm, btn_secondTerm
+var btn_upperSection, btn_lowerSection
+var selected_grade
+
+var isLowerSection = false
 
 var subject = [
-	{id:["200-101"], name: ["INTRO TO ENGINEERING"], time: 1, grade: "D"},
-	{id:["240-101","242-101"], name: ["INTRO TO COMPUTER PROGRAM"], time: 3, grade: "D"},
-	{id:["322-171"], name: ["PHYSICAL SCIENCE MATH I"], time: 3, grade: "D"},
-	{id:["332-103"], name: ["PHYSICS FOR ENGINEERING I"], time: 3, grade: "D"},
-	{id:["332-113"], name: ["PHYSICS LAB FOR ENGINEERING I"], time: 1, grade: "D"},
-	{id:["890-002"], name: ["EVERYDAY ENGLISH"], time: 2, grade: "D"},
-	{id:["215-111","216-111"], name: ["ENGINEERING DRAWING"], time: 3, grade: "D"},
-	{id:["221-101","220-102"], name: ["ENGINEERING MECHANICS I"], time: 3, grade: "D"},
-	{id:["322-172"], name: ["PHYSICAL SCIENCE MATH II"], time: 3, grade: "D"},
-	{id:["324-103"], name: ["GENERAL CHEMISTRY"], time: 3, grade: "D"},
-	{id:["325-103"], name: ["GENERAL CHEMISTRY LAB"], time: 1, grade: "D"},
-	{id:["332-104"], name: ["PHYSICS FOR ENGINEERING II"], time: 3, grade: "D"},
-	{id:["332-114"], name: ["PHYSICS LAB FOR ENGINEERING II"], time: 1, grade: "D"},
-	{id:["890-003"], name: ["ENGLISH ON THE GO"], time: 2, grade: "D"},
-	{id:["890-004"], name: ["ENGLISH IN THE DIGITAL WORLD"], time: 2, grade: "D"},
-	{id:["890-005"], name: ["ENGLISH FOR ACADEMIC SUCCESS"], time: 2, grade: "D"},
+	{
+		id:["200-101"],
+		name: ["INTRO TO ENGINEERING"],
+		time: 1, grade: "D"
+	},
+	{
+		id:["240-101","242-101"],
+		name: ["INTRO TO COMPUTER PROGRAM"],
+		time: 3, grade: "D"
+	},
+	{
+		id:["322-171"],
+		name: ["PHYSICAL SCIENCE MATH I"],
+		time: 3, grade: "D"
+	},
+	{
+		id:["332-103"],
+		name: ["PHYSICS FOR ENGINEERING I"],
+		time: 3, grade: "D"
+	},
+	{
+		id:["332-113"],
+		name: ["PHYSICS LAB FOR ENGINEERING I"],
+		time: 1, grade: "D"
+	},
+	{
+		id:["890-002","890-003","890-004"],
+		name: ["EVERYDAY ENGLISH","ENGLISH ON THE GO","ENGLISH IN THE DIGITAL WORLD"],
+		time: 2, grade: "D"
+	},
+	{
+		id:["215-111","216-111"],
+		name: ["ENGINEERING DRAWING"],
+		time: 3, grade: "D"
+	},
+	{
+		id:["221-101","220-102"],
+		name: ["ENGINEERING MECHANICS I"],
+		time: 3, grade: "D"
+	},
+	{
+		id:["322-172"],
+		name: ["PHYSICAL SCIENCE MATH II"],
+		time: 3, grade: "D"
+	},
+	{
+		id:["324-103"],
+		name: ["GENERAL CHEMISTRY"],
+		time: 3, grade: "D"
+	},
+	{
+		id:["325-103"],
+		name: ["GENERAL CHEMISTRY LAB"],
+		time: 1, grade: "D"
+	},
+	{
+		id:["332-104"],
+		name: ["PHYSICS FOR ENGINEERING II"],
+		time: 3, grade: "D"
+	},
+	{
+		id:["332-114"],
+		name: ["PHYSICS LAB FOR ENGINEERING II"],
+		time: 1, grade: "D"
+	},
+	{
+		id:["890-003","890-004","890-005"],
+		name: ["ENGLISH ON THE GO","ENGLISH IN THE DIGITAL WORLD","ENGLISH FOR ACADEMIC SUCCESS"],
+		time: 2, grade: "D"
+	}
 ]
 
-function onClick(num)
+window.onload = function()
 {
-	if(num == "1")
-		firstTerm = !firstTerm;
-	else if(num == "2")
-		secondTerm = !secondTerm;
-	
-	onUpdate();
-}
+	btn_firstTerm = document.getElementById('btn_firstTerm')
+	btn_secondTerm = document.getElementById('btn_secondTerm')
 
-function load()
-{
-	onClick("1");
-	onClick("2");
+	btn_upperSection = document.getElementById('btn_upperSection')
+	btn_lowerSection = document.getElementById('btn_lowerSection')
+
+	btn_firstTerm.addEventListener("click", function()
+	{
+		firstTerm = !firstTerm
+		onUpdate()
+	})
+	btn_secondTerm.addEventListener("click", function()
+	{
+		secondTerm = !secondTerm
+		onUpdate()
+	})
+	btn_upperSection.addEventListener("click", function()
+	{
+		btn_lowerSection.disabled = false;
+		btn_upperSection.disabled = true;
+		isLowerSection = false
+		onUpdate()
+	})
+	btn_lowerSection.addEventListener("click", function()
+	{
+		btn_lowerSection.disabled = true;
+		btn_upperSection.disabled = false;
+		isLowerSection = true
+		onUpdate()
+	})
+
+	onUpdate()
 }
 
 function onUpdate()
@@ -163,8 +241,8 @@ function createDiv(index, section)
 	
 	var gradeDiv = document.createElement('div');
 	gradeDiv.style.width = "17%";
-	gradeDiv.className = "table_cell member_cell round_border";
-	gradeDiv.innerHTML = subject[index].grade;
+	gradeDiv.className = "table_cell member_cell round_border custom_select";
+	gradeDiv.appendChild(createSelect())
 	
 	d.appendChild(idDiv);
 	d.appendChild(nameDiv);
@@ -172,4 +250,29 @@ function createDiv(index, section)
 	d.appendChild(gradeDiv);
 	
 	return d;
+}
+
+function createSelect()
+{
+	var gradeSelect = document.createElement('select')
+	insertOption(gradeSelect, "", "")
+	insertOption(gradeSelect, "A", "A")
+	insertOption(gradeSelect, "B+", "B+")
+	insertOption(gradeSelect, "B", "B")
+	insertOption(gradeSelect, "C+", "C+")
+	insertOption(gradeSelect, "C", "C")
+	insertOption(gradeSelect, "D+", "D+")
+	insertOption(gradeSelect, "D", "D")
+	insertOption(gradeSelect, "E", "E")
+	insertOption(gradeSelect, "I", "I")
+	insertOption(gradeSelect, "Withdrawn", "W")
+	insertOption(gradeSelect, "ไม่ลงทะเบียน", "")
+	return gradeSelect;
+}
+
+function insertOption(select, txt, val)
+{
+	var option = document.createElement('option');
+	option.text = txt; option.value = val;
+	select.add(option)
 }
